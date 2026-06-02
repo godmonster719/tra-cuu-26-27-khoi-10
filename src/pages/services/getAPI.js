9821e4ad7907a0bd32d9f01acb97ae5492dca5ed
Fire Laptop <kirellia719@gmail.com>
2026-06-02 08:18:10 +0700

    thêm API cho khối 10

--- src/pages/services/getAPI.js
+++ src/pages/services/getAPI.js
@@ -2,6 +2,9 @@ const API_LIST = [
     "https://tuyen-sinh-26-27-be-10.onrender.com",
     "https://tuyen-sinh-26-27-be-10-1.onrender.com",
     "https://tuyen-sinh-26-27-be-10-2.onrender.com",
+    "https://tuyen-sinh-26-27-be-10.vercel.app",
+    "https://tuyen-sinh-26-27-be-10-4ryp.vercel.app",
+    "https://tuyen-sinh-26-27-be-10-8tbn.vercel.app",
 ];
 
 const getRandomApi = () => {
